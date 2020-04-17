baf1e04e533d4dc895dac0c59d134b34f94beb6c
sunreet singh <z5130780@student.unsw.edua.au>
2020-04-17 22:56:04 +1000

    people served feature

--- api.py
+++ api.py
@@ -21,6 +21,7 @@ CHEQUES='C'
 EXCHANGE='D'
 GENERAL_INFO='E'
 ATM='F'
+num_people_served = 0;
 
 app = Flask(__name__)
 CORS(app)
@@ -33,6 +34,7 @@ cur.execute('create table if not exists dynamic_queue (`index` int, name varchar
 cur.execute('create table if not exists analytics (`index` int, name varchar, customer_id varchar, service varchar, ticket varchar, counter varchar)')
 cur.execute('create table if not exists customers (`index` int, name varchar, customer_id varchar, password varchar)')
 cur.execute('create table if not exists feedbacks (`index` int, customer_id varchar, feedback varchar)')
+
 conn.commit()
 conn.close()
 
@@ -158,6 +160,7 @@ class Collections(Resource):
 @api.route('/queue/<ticket>', methods=['DELETE'])
 class delete(Resource):
 	def delete(self, ticket):
+
 		conn = sqlite3.connect('data.db')
 		cur = conn.cursor()
 
@@ -172,9 +175,19 @@ class delete(Resource):
 
 		cur.execute('Delete from dynamic_queue where ticket ='+ticket)
 		conn.commit()
+		num_people_served = num_people_served +1;
 		conn.close()
 
-		return 200
+		return res, 200
+
+@api.route('/show/servedpeople', methods=['GET'])
+class GetInfo(Resource):
+	def get(self):
+		res ={
+			"num_people_served": str(num_people_served)
+		}
+
+		return res, 200
 
 @api.route('/show/alltickets', methods=['GET'])
 class GetInfo(Resource):
