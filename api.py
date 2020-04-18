ead61d65603dc35083968dce4bdd218288395658
sunreet singh <z5130780@student.unsw.edua.au>
2020-04-18 16:23:50 +1000

    no name

--- api.py
+++ api.py
@@ -64,7 +64,7 @@ class Collections(Resource):
 				cur.execute('select * from customers where customer_id = "'+str(cust)+'"')
 				result = cur.fetchall()
 				for i in result:
-					print(type(result[0]))
+					print(result[0][0])
 					#print(result['name'])
 
 					#df['name'] = result[]
