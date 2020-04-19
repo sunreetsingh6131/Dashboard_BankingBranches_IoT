875d2f4d6029f36127b87f4628b12b2f5609300c
sunreet singh <z5130780@student.unsw.edua.au>
2020-04-19 14:18:25 +1000

    datetime addition

--- api.py
+++ api.py
@@ -526,7 +526,7 @@ class GetInfo(Resource):
 		print("ALL TIMESTAMPS")
 		print(result)
 
-		cur.execute('select count(*) from timelog where time between "2020-04-19 04:00:00" AND "2020-04-19 04:10:00"')
+		cur.execute('select count(*) from timelog where time between "2020-04-19 04:00:00" AND "2020-04-19 04:16:00"')
 		result = cur.fetchall()
 		print(result)
 
