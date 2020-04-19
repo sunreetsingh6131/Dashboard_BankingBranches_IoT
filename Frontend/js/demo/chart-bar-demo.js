3591a5447f4c9ebaf113a2c4e3919d1fb23c4dc5
sunreet singh <z5130780@student.unsw.edua.au>
2020-04-19 14:41:52 +1000

    timestamps

--- Frontend/js/demo/chart-bar-demo.js
+++ Frontend/js/demo/chart-bar-demo.js
@@ -1,61 +1,64 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = 'black';
 
-fetch('http://34.87.233.248:5000/show/timelogs')
+fetch('http://34.87.233.248:5000/show/logs')
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    var ctx = document.getElementById("barChart").getContext('2d');
-    var data = {
-      labels: ["9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"],
-      datasets: [{
-          label: "People per hour",
-          // fill: true,
-          lineTension: 0.1,
-          backgroundColor: "rgba(54, 185, 204,0.2)",
-          borderColor: "#36b9cc",
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: "white",
-          pointBackgroundColor: "black",
-          pointBorderWidth: 1,
-          pointHoverRadius: 8,
-          pointHoverBackgroundColor: "brown",
-          pointHoverBorderColor: "yellow",
-          pointHoverBorderWidth: 2,
-          pointRadius: 4,
-          pointHitRadius: 10,
-          // notice the gap in the data and the spanGaps: false
-          data: data['time'],
-          spanGaps: false,
-        }
-      ]
-    };
-    var options = {
-    scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  },
-                  scaleLabel: {
-                       display: true,
-                       labelString: 'No. of people',
-                       fontSize: 20 
-                    }
-              }]            
-          }  
-    };
-
-  // Chart declaration:
-    var myBarChart = new Chart(ctx, {
-      type: 'line',
-      data: data,
-      options: options
-    });
+    console.log(data['time'])
   };
 
 
+var ctx = document.getElementById("barChart").getContext('2d');
+
+var data = {
+  labels: ["9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"],
+  datasets: [{
+      label: "People per hour",
+      // fill: true,
+      lineTension: 0.1,
+      backgroundColor: "rgba(54, 185, 204,0.2)",
+      borderColor: "#36b9cc",
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: "white",
+      pointBackgroundColor: "black",
+      pointBorderWidth: 1,
+      pointHoverRadius: 8,
+      pointHoverBackgroundColor: "brown",
+      pointHoverBorderColor: "yellow",
+      pointHoverBorderWidth: 2,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      // notice the gap in the data and the spanGaps: false
+      data: [10, 20, 60, 95, 64, 78, 90,20,70,40,70,89],
+      spanGaps: false,
+    }
+  ]
+};
+
 // Notice the scaleLabel at the same level as Ticks
+var options = {
+  scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                },
+                scaleLabel: {
+                     display: true,
+                     labelString: 'No. of people',
+                     fontSize: 20 
+                  }
+            }]            
+        }  
+};
+
+// Chart declaration:
+var myBarChart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: options
+});
